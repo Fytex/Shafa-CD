44eb7523d13fc911faebda47849b8b6485555b2a
leonardo1924 <leonardo1924@protonmail.com>
2020-12-07 19:46:36 +0000

    Added insertSort, merge and timSort on t.c

--- src/modules/t.c
+++ src/modules/t.c
@@ -24,6 +24,73 @@ typedef struct Symbol
 Symbol symbols[256];
 
 // uint_8 * output[256];
+void insertSort (Symbol * symbols, int left , int right)
+{
+    for (int i = left +1; i<= right ; i++ ){
+
+        uint32_t tmpfreq = symbols[i].freq;
+        Symbol freq = symbols[i];
+        int j = i - 1;
+
+        while (j >= left && symbols[j].freq < tmpfreq  ){
+            symbols[j+1] = symbols[j];
+            j--;
+        }
+        symbols[j+1] = freq;
+    }
+} 
+
+void merge (Symbol * symbols, int l, int m, int r) 
+{ 
+      
+    int len1 = m - l + 1, len2 = r - m; 
+    Symbol left[len1], right[len2]; 
+    for (int i = 0; i < len1; i++) 
+        left[i] = symbols[l + i]; 
+    for (int i = 0; i < len2; i++) 
+        right[i] = symbols[m + 1 + i]; 
+  
+    int i = 0; 
+    int j = 0; 
+    int k = l; 
+  
+    while (i < len1 && j < len2){ 
+        if (left[i].freq >= right[j].freq){ 
+            symbols[k] = left[i]; 
+            i++; 
+        } 
+        else{
+            symbols[k] = right[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+    for (;i<len1;i++)symbols[k++] = left[i];
+    for (;j<len2;j++)symbols[k++] = right[j];
+    
+}  
+
+void timSort(Symbol* symbols) 
+{ 
+    int n = nsimb;
+    // Sort individual subarrays of size RUN.
+    for (int i = 0; i < n; i+=RUN) 
+        insertSort(symbols, i, min((i+31),(n-1))); 
+  
+    // Start merging from size RUN (or 32).  
+    for (int size = RUN; size < n;size = 2*size) 
+    {
+
+        for (int left = 0; left < n;left += 2*size) 
+        {// find ending point of left sub array and mid+1 is starting point of right sub array.
+            int mid = left + size - 1; 
+            int right = min((left + 2*size - 1),(n-1)); 
+  
+            // merge sub array left with sub array right.
+            merge(symbols, left, mid, right); 
+        } 
+    } 
+} 
 
 void readBlock (FILE* f) {
     int blockSize;  

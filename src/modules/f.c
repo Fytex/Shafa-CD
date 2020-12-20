7fe2befb4dd33c3f13d9426a251fc4ecb12bf869
Utilizador <joaopedrocarvalhovnf@gmail.com>
2020-12-20 21:58:12 +0000

    fixed bugs and changes  in writing rle file

--- src/modules/f.c
+++ src/modules/f.c
@@ -7,24 +7,27 @@
  **************************************************/
 
 #include "f.h"
-#include "errors.h"
+#include "utils/errors.h"
+#include "utils/file.h"
+#include "utils/extensions.h"
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-int block_compression(char * buffer, char *block, const int block_size, int size_f)
+int block_compression(uint8_t buffer[], uint8_t block[], const int block_size, int size_f)
 {
     //Looping variables(i,j,k,l), number of symbols int the beginning(i) and if compressed(comp_block_size)
-    int i, j, l, size_block_rle;
+    int i, j, size_block_rle;
     //Cycle that goes through the first block of symbols of the file
     for(i = 0, size_block_rle=0; i < block_size && i < size_f; i = j) {
         int n_reps = 0; //Number of repetitions of a symbol
-        for(j = i; buffer[i] == buffer[j]; ++j, ++n_reps); //Counts the number of repetitions of a symbol
+        for(j = i; buffer[i] == buffer[j] && n_reps <255; ++j, ++n_reps); //Counts the number of repetitions of a symbol
         if(n_reps >= 4 || !buffer[i]) //If a symbol repits itself 4 times or more or if the symbol is null
         {
-            block[size_block_rle] = NULL;
+            block[size_block_rle] = 0;
             block[size_block_rle+1] = buffer[i];
             block[size_block_rle+2] = n_reps;
             size_block_rle +=3;
@@ -39,7 +42,7 @@ int block_compression(char * buffer, char *block, const int block_size, int size
     return size_block_rle;
 }
 
-algo make_freq_txt(char* buffer)
+/* algo make_freq_txt(char* buffer)
 {
     if(f)
     {
@@ -78,59 +81,85 @@ algo make_freq_rle(char* block)
         }
     }
 }
-
+*/
 
 _modules_error freq_rle_compress(char** const path, const bool force_rle, const int block_size)
 {
     //Reading and loading file into the buffer
-    char* file_name = *path;
-    FILE* f = fopen(file_name, "rb");
+    FILE* f = fopen(*path, "rb");
+    bool compress_rle = true;
     if(!f) return _FILE_INACCESSIBLE;
-    
+    unsigned long * the_block_size = block_size;
+    long *size_of_last_block = 0;
+    long long n_blocks = fsize(f, *path, &the_block_size, &size_of_last_block);
 
-    fseek(f, 0, SEEK_END); //Goes to the end of the file
-    int size_f = ftell(f); //Saves the size of the file
+    int size_f = (n_blocks-1) * (int)the_block_size + (int)size_of_last_block; //Saves the size of the file
+    //int size_f = ftell(f); 
     rewind(f); //Goes to the begining of the file
 
-    if(size_f < 1024 && !force_rle) return _FILE_TOO_SMALL; //If the file is too small
 
-    char* buffer = malloc(sizeof(char) * size_f); //Allocates memory for all the characters in the file
-    if(!buffer) return _LACK_OF_MEMORY;
+
+    if(size_f < 1024) {
+        fclose(f);
+        return _FILE_TOO_SMALL;  //If the file is too small
+    }
+
+    uint8_t * buffer = malloc(size_f); //Allocates memory for all the characters in the file
+    if(!buffer) {
+        fclose(f);
+        return _LACK_OF_MEMORY;
+    }
 
     int read = fread(buffer, 1, size_f, f); //Loads the content of the file into the buffer
-    if (read != size_f) return _FILE_CORRUPTED;
+    if (read != size_f) return _FILE_STREAM_FAILED;
 
     fclose(f);
 
     //Writing in a new file
-    FILE* f_rle = fopen("new.rle", "rb");
+    FILE* f_rle = fopen("new.rle", "wb");
     if(!f_rle) return _FILE_INACCESSIBLE;
 
-    char* block = malloc(sizeof(char) * block_size); //Allocates memory for the compressed(or not) content of the file
-    if(!block) return _LACK_OF_MEMORY;
+    FILE* f_freq = fopen("new.freq", "wb");
+    if(!f_freq) return _FILE_INACCESSIBLE;
+
+    uint8_t * block;
 
     // divides and compresses the buffer into blocks and writes it into the rle file
-    int num_blocks=0, compresd, size_block_rle = 0;
-    for(int s = 0; s < size_f; ++num_blocks) {
-        if(size_f - s + block_size < 1024 || size_f - s < block_size) // if the block is shorter than the default block size or if the last block is smaller than 1KB 
-        {
-            compresd = size_f - s; // real block size
+    int compresd = (int)the_block_size, size_block_rle = 0;
+    for (int block_num = 0, s = 0; block_num < n_blocks; ++block_num) {
+        if(block_num == n_blocks -1) {
+            compresd = size_f - s;
+        }
+        block = malloc(compresd * 3); //creates string with enough memory to compress the block
+        if(!block) {
+            fclose(f_freq);
+            fclose(f_rle);
+            return _LACK_OF_MEMORY;
+        }
+        size_block_rle = block_compression(buffer+s, block, compresd, size_f); 
+        if(block_num == 0 ) {
+            double compression_ratio = (double)(compresd - size_block_rle)/compresd; //Calculates the compression ratio
+            if(compression_ratio < 0.05 && !force_rle) compress_rle = false;
+            //write freq header
+        }
+        if(compress_rle) {
+            /*if(block_num == 0) {
+                FILE* f_rle = fopen("new.rle", "wb");
+                if(!f_rle) return _FILE_INACCESSIBLE;
+            }
+            */ 
+            int res = fwrite(block, 1, size_block_rle, f_rle);
+            //onde tratar das freq do rle
+            free(block);
         }
         else {
-            compresd = block_size;
+            //freq do original
+            free(block);
         }
-        block = malloc(sizeof(char) * compresd); //creates string with enough memory to compress the block
-        size_block_rle = block_compression(buffer+s, block, compresd, size_f); 
-        if(num_blocks == 0) {
-            double compression_ratio = (compresd - size_block_rle)/compresd; //Calculates the compression ratio
-            if(compression_ratio < 0.05 && !force_rle) return _FILE_TOO_SMALL;
-        }
-        fwrite(block, 1, size_block_rle, f_rle);
-        //onde tratar das freq
-        free(block);
         s+=compresd;
     }
-
+    
+    fclose(f_freq);
     fclose(f_rle);
 
     return _SUCCESS;

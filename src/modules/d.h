8a324ced67466cfd74062d6ece806d18e2f0c2d7
Fytex <pedropauloqt@gmail.com>
2020-12-28 00:18:40 +0000

    module d header file added internal and external function for rle decompression

--- src/modules/d.h
+++ src/modules/d.h
@@ -5,9 +5,6 @@
 
 #include "utils/errors.h"
 
-#define rle_decompress(path) _rle_decompress(path, NULL)
-
-
 typedef struct {
     unsigned long * sizes;
     long long length;
@@ -26,12 +23,22 @@ _modules_error _shafa_decompress(char ** path, bool decompress_rle);
 /**
 \brief Decompresses file which was compressed with RLE's algorithm and saves it to disk.
 This is an internal function which can be used in specific cases where the programmer already knows the size of each block from Original's file.
-For simple goals use `rle_decompress(path)` macro. 
  @param path Pointer to the original/RLE file's path
  @param blocks_size Blocks Size struct
  @returns Error status
 */
 _modules_error _rle_decompress(char ** path, const BlocksSize blocks_size);
+
+
+/**
+\brief Decompresses file which was compressed with RLE's algorithm and saves it to disk.
+ @param path Pointer to the original/RLE file's path
+ @returns Error status
+*/
+static inline _modules_error rle_compress(char ** path)
+{
+    return _rle_compress(path, (BlocksSize) {NULL, 0});
+}
 
 
 #endif //MODULE_D_H

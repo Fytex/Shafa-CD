5372adabe9c116f6afb29beffd57a36af9df0cbf
Fytex <pedropauloqt@gmail.com>
2020-12-30 18:33:17 +0000

    Decompressed check

--- src/shafa.c
+++ src/shafa.c
@@ -131,7 +131,7 @@ _modules_error execute_modules(Options options, char ** const ptr_file) // bette
 {
     _modules_error error;
     char * tmp_file;
-    bool file_rle_shaf = false;
+    bool file_rle_shaf = false, decompressed = false;
     
     if (options.module_f) {
         error = freq_rle_compress(ptr_file, options.f_force_rle, options.f_force_freq, options.block_size); // Returns true if file was RLE compressed
@@ -215,10 +215,12 @@ _modules_error execute_modules(Options options, char ** const ptr_file) // bette
                     fputs("Module d: Something went wrong while decompressing...\n", stderr);
                     return error;
                 }
+                else
+                    decompressed = true;
             }
         }
         
-        if (options.d_rle && !options.d_shaf) { // Trigger: -m d -d r
+        if (!decompressed && (options.d_rle || !options.d_shaf) { // Trigger: NULL | -m d | -m d -d r (Won't execute this statement if had already been decompressed) 
 
             if (!check_ext(*ptr_file, RLE_EXT)) { 
                 fprintf(stderr, "Module d: Wrong extension... Should end in %s\n", RLE_EXT);

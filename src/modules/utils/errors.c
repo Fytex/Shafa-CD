68fc91f29da604b6ec211f7183b2c29d8e35593a
Fytex <pedropauloqt@gmail.com>
2020-12-30 18:56:18 +0000

    Update errors.c

--- src/modules/utils/errors.c
+++ src/modules/utils/errors.c
@@ -14,7 +14,7 @@
     _(     _LACK_OF_MEMORY, "Not enough memory for allocation\n"                                          )     \
     _(  _FILE_INACCESSIBLE, "File can't be accessed. Either lack of permissions or file doesn't exist\n"  )     \
     _(_FILE_UNRECOGNIZABLE, "File not recognized\n"                                                       )     \
-    _( _FILE_STREAM_FAILED, "Can't communicate properly with file's stream"                               )     \
+    _( _FILE_STREAM_FAILED, "Can't communicate properly with file's stream\n"                             )     \
     _(     _FILE_TOO_SMALL, "File too small for decompression\n"                                          )
     
 

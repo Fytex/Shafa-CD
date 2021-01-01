405b042d59f55a5c5f457adcfc0a42b6d42510c4
leonardo1924 <leonardo1924@protonmail.com>
2021-01-01 20:13:53 +0000

    Added coment's to module B

--- src/modules/t.c
+++ src/modules/t.c
@@ -18,10 +18,17 @@
 #define NUM_SYMBOLS 256
 #define MIN(a,b) ((a) < (b) ? a : b)
 
+/**
+ * @brief Function that reads the blocks provided by the freq file 
+ * 
+ * @param codes_input buffer of respective file.freq block
+ * @param frequencies Array to store the freq from the symbol
+ * @return _modules_error 
+ */
 static _modules_error read_Block(char * restrict codes_input, unsigned long * restrict frequencies) 
 {
     int read_count;
-
+    //check if something is read in relation to the frequencies of the .freq file
     if (sscanf(codes_input, "%lu%n;", frequencies, &read_count) != 1)
         return _FILE_UNRECOGNIZABLE;
 
@@ -50,6 +57,14 @@ static _modules_error read_Block(char * restrict codes_input, unsigned long * re
     return _SUCCESS;
 }
 
+/**
+ * @brief Fuction to sort the frequencies array in descending order
+ * 
+ * @param frequencies The array we used
+ * @param positions Array with the Symbols Positions
+ * @param start Begining of the array 
+ * @param end  Ending of the array
+ */
 static void insert_sort (unsigned long frequencies[], int positions[], int left, int right)
 {
     unsigned long tmpFreq;
@@ -60,6 +75,9 @@ static void insert_sort (unsigned long frequencies[], int positions[], int left,
         tmpFreq = frequencies[i];
         j = i - 1;
 
+        /* Move elements of frequencies[0..i-1], that are greater
+        than tmpFreq, to one position ahead of their current position
+        */
         while (j >= left && frequencies[j] < tmpFreq){
             frequencies[j+1] = frequencies[j];           
             --j;
@@ -67,7 +85,9 @@ static void insert_sort (unsigned long frequencies[], int positions[], int left,
         frequencies[j+1] = tmpFreq;
         a = j + 1;        
         iters = i - a;
-
+         /* Do the same thing to position so that the 
+        frequency value associated with each symbol is not lost
+        */
         for (int idx = i-1; iters; --idx)
             if (positions[idx] >= a) {
                 ++positions[idx];
@@ -77,7 +97,16 @@ static void insert_sort (unsigned long frequencies[], int positions[], int left,
     }
 } 
 
-
+/**
+ * @brief Function that calculates 
+ * the sum of the positional frequencies first 
+ * to the last position.
+ * 
+ * @param frequencies Array Frequencies 
+ * @param first First Element of the array
+ * @param last Last element of the array
+ * @return unsigned long 
+ */
 static unsigned long sum_Freq (unsigned long frequencies[], int first, int last)
 {
     unsigned long sum = 0;
@@ -88,7 +117,15 @@ static unsigned long sum_Freq (unsigned long frequencies[], int first, int last)
     return sum;
 }
 
-
+/**
+ * @brief The function is used to calculate the best division of 
+ * any sequence of frequencies ordered between the element in position first and the element in position last, as long as last > first.
+ * 
+ * @param frequencies Array Frequencies 
+ * @param first First element of the array
+ * @param last  Last element of the array
+ * @return int 
+ */
 static int best_Division (unsigned long frequencies[], int first, int last)
 {
     
@@ -113,21 +150,37 @@ static int best_Division (unsigned long frequencies[], int first, int last)
     return division - 1;
 }
 
+/**
+ * @brief Function that will add the bit 0 or 1 to the code according to the Sf algorithm
+ * 
+ * @param value bit will be added to code 0 or 1
+ * @param codes Array to store the sf_codes
+ * @param start First Element
+ * @param end Last Element 
+ */
 static void add_bit_to_code(char value, char codes[NUM_SYMBOLS][NUM_SYMBOLS], int start, int end)
 {
     char * symbol_codes;
-
+    /*For symbol_codes, start the pointer on the first element, 
+    going through the array until the end */
     for ( ; start <= end; ++start) {
         symbol_codes = codes[start];
-
+        //While the pointer is moved it adds value 1 or 0. This step is made by this for
         for ( ; *symbol_codes; ++symbol_codes);
         *symbol_codes = value;
     }
 }
 
-
+/**
+ * @brief Function that applies the shannon fanon algorithm
+ * 
+ * @param frequencies Array with frequencies
+ * @param codes Array to store the codes after SF
+ * @param start First element of the array
+ * @param end Last element of the array
+ */
 static void sf_codes (unsigned long frequencies[], char codes[NUM_SYMBOLS][NUM_SYMBOLS], int start, int end)
-{
+{//While the pointer at the beginning is not the same as at the end it applies the algorithm
     if (start != end){
                
         int div = best_Division(frequencies, start, end);
@@ -140,15 +193,29 @@ static void sf_codes (unsigned long frequencies[], char codes[NUM_SYMBOLS][NUM_S
     }
 }
 
+/**
+ * @brief Function that counts how many non-null elements there are
+ * 
+ * @param frequencies Array for check
+ * @return int 
+ */
 static int not_null (unsigned long frequencies[NUM_SYMBOLS])
 {
     int r = 0;
+     //Checks for null elements in the frequencies array and updates the r when it finds one
     for (int i = NUM_SYMBOLS - 1; frequencies[i] == 0; --i) ++r;
 
     return (NUM_SYMBOLS - 1 - r);
 }
 
-
+/**
+ * @brief Gives print in the screen of all the information related to this module
+ * 
+ * @param num_blocks Number of Blocks in the freq file
+ * @param sizes Array with all block sizes
+ * @param total_time Time it took to execute the module 
+ * @param path Original/RLE file's path
+ */
 static inline void print_summary(const long long num_blocks, const unsigned long * sizes, const double total_time, const char * const path)
 {
     long long i;
@@ -161,6 +228,7 @@ static inline void print_summary(const long long num_blocks, const unsigned long
             "Size of blocks analyzed in the symbol file: " ,
             num_blocks 
     );
+    //For -> Save the sizes of all blocks in an array and then print on the interface
     for (i = 0; i < num_blocks - 1; ++i) {
         printf("%lu/", sizes[i]);
     }
@@ -175,7 +243,12 @@ static inline void print_summary(const long long num_blocks, const unsigned long
 
 
 // This module should receive a .freq file, but shafa.c will handle that file and pass to this module the original file
-
+/**
+ * @brief Get the shafa codes object/Creates a table of Shanon Fano's codes and saves it to disks
+ * 
+ * @param path Original/RLE file's path
+ * @return _modules_error 
+ */
 _modules_error get_shafa_codes(const char * path)
 {
     clock_t t;
@@ -202,11 +275,11 @@ _modules_error get_shafa_codes(const char * path)
         fd_freq = fopen(path_freq, "rb");
 
         if (fd_freq) {
-
+             // Reading header of freq file
             if (fscanf(fd_freq, "@%c@%lld", &mode, &num_blocks) == 2) {                
-
+                // Checking the mode of the file
                 if (mode == 'R' || mode == 'N') {
-
+                // Allocates memory to an array with the purpose of saving the sizes of each RLE/TXT block
                     sizes = malloc (num_blocks * sizeof(unsigned long));
    
                     if (sizes) {                    
@@ -288,7 +361,8 @@ _modules_error get_shafa_codes(const char * path)
                                 }
                                 else 
                                     error = _FILE_STREAM_FAILED;
-
+                                /*If fd_codes have errors the program returns a warning
+                                 matches the errors present in the else of this if*/
                                 if (!error)
                                     fprintf(fd_codes, "@0");
 
@@ -321,8 +395,9 @@ _modules_error get_shafa_codes(const char * path)
         free(path_freq);
     }
     else
-        error = _LACK_OF_MEMORY;  
-
+        error = _LACK_OF_MEMORY;          
+      /*If no error occurred during the execution of the module, the time taken to execute it is counted 
+    and the information related to it is printed (print_summary)*/
     if (!error) {
         t = clock() - t;
         total_time = (((double) t) / CLOCKS_PER_SEC) * 1000;
